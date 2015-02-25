29f2598be40e5f87ab3dbcb3d56c09c61a8ea09d
malinky <applemacbook@Craigs-MacBook.local>
2015-02-25 08:34:19 +0000

    Update gulp with minify css

--- gulpfile.js
+++ gulpfile.js
@@ -6,33 +6,22 @@ var gulp            = require('gulp');
 
 var autoprefixer    = require('gulp-autoprefixer');
 var del             = require('del');
+var minifyCSS       = require('gulp-minify-css');
 var runSequence     = require('run-sequence');
-var sourcemaps      = require('gulp-sourcemaps');
 var uglify          = require('gulp-uglify');
 
 /*
 Browser List for Autoprefixer https://github.com/ai/browserslist
-https://github.com/sindresorhus/del
 https://www.npmjs.com/package/del
+https://github.com/sindresorhus/del
+https://www.npmjs.com/package/gulp-minify-css
+https://github.com/jonathanepollack/gulp-minify-css
 https://github.com/OverZealous/run-sequence
 https://www.npmjs.com/package/run-sequence
-https://github.com/floridoo/gulp-sourcemaps
-https://www.npmjs.com/package/gulp-sourcemaps
 https://github.com/terinjokes/gulp-uglify
 https://www.npmjs.com/package/gulp-uglify
 https://github.com/terinjokes/gulp-uglify/issues/56
 */
-
-
-
-
-
-/* ------------------------------------------------------------------------ *
- * Local
- * ------------------------------------------------------------------------ */
-
-
-
 
 
 /* ------------------------------------------------------------------------ *
@@ -41,8 +30,8 @@ https://github.com/terinjokes/gulp-uglify/issues/56
  * gulp dev
  *
  * Move all applicable files and folders.
- * This includes and all js for debugging with sourcemaps.
- * Concat and minify JS to scripts.js
+ * Minify CSS, Autoprefix.
+ * Minify JS.
  * ------------------------------------------------------------------------ */
 
 /**
@@ -64,39 +53,33 @@ gulp.task('dev-move-files', function() {
 
 /**
   * Move root directories and their contents.
-  * Move css, img and js to be used with root maps on dev.
+  * Move img only.
+  * No sourcemaps used so no need to move css and js folders.
   */
 gulp.task('dev-move-dir', function() {
-    return gulp.src(['css/**', 'img/**', 'js/**'], { base: './'} )
+    return gulp.src('img/**', { base: './'} )
         .pipe(gulp.dest('dev'));
 });
 
 
 /**
- * Autoprefix vendors.
- * Ensure SASS is complete first with a dependancy.
+ * Minify CSS, Autoprefix.
  */
-gulp.task('dev-autoprefix', function () {
-    return gulp.src('dev/css/style.css')
-        .pipe(autoprefixer({
-            browsers: ['last 5 versions']
-        }))
-        .pipe(gulp.dest('dev/css'));
+gulp.task('dev-styles', function() {
+    return gulp.src('css/*.css')
+    .pipe(autoprefixer({browsers: ['last 5 versions']}))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dev/css'));
 });
 
 
 /**
- * Minify our JS.
- *
- * sourceRoot sets the path where the source files are hosted relative to the source map.
- * This makes things appear in the correct folders when viewing through developer tools.
+ * Minify JS.
  */
 gulp.task('dev-scripts', function() {
     return gulp.src('js/*.js')
-		.pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('../sourcemaps', {includeContent: false, sourceRoot: '../js'}))
-        .pipe(gulp.dest('dev/js'));
+    .pipe(uglify())
+    .pipe(gulp.dest('dev/js'));
 });
 
 
@@ -104,16 +87,13 @@ gulp.task('dev-scripts', function() {
  * Set up dev task.
  */
 gulp.task('dev', function() {
-  	runSequence('dev-clean', 
+    runSequence('dev-clean', 
                 'dev-move-files', 
                 'dev-move-dir', 
-                'dev-autoprefix', 
+                'dev-styles', 
                 'dev-scripts'
             );
 })
-
-
-
 
 
 /* ------------------------------------------------------------------------ *
@@ -122,7 +102,8 @@ gulp.task('dev', function() {
  * gulp prod
  *
  * Move all applicable files and folders.
- * Concat and minify JS to scripts.js.
+ * Minify CSS, Autoprefix.
+ * Minify JS.
  * ------------------------------------------------------------------------ */
 
 /**
@@ -134,7 +115,7 @@ gulp.task('prod-clean', function (cb) {
 
 
 /**
-  * Move root .php files
+  * Move root .php files.
   */
 gulp.task('prod-move-files', function() {
     return gulp.src('*.php')
@@ -144,34 +125,33 @@ gulp.task('prod-move-files', function() {
 
 /**
   * Move root directories and their contents.
-  * Not js as we just need minified version as no sourcemaps are used in prod.
+  * Move img only.
+  * No sourcemaps used so no need to move css and js folders.
   */
 gulp.task('prod-move-dir', function() {
-    return gulp.src(['css/**', 'img/**'], { base: './'} )
+    return gulp.src('img/**', { base: './'} )
         .pipe(gulp.dest('prod'));
 });
 
 
 /**
- * Autoprefix vendors.
- * Ensure SASS is complete first with a dependancy.
+ * Minify CSS, Autoprefix.
  */
-gulp.task('prod-autoprefix', function () {
-    return gulp.src('prod/css/style.css')
-        .pipe(autoprefixer({
-            browsers: ['last 5 versions']
-        }))
-        .pipe(gulp.dest('prod/css'));
+gulp.task('prod-styles', function() {
+    return gulp.src('css/*.css')
+    .pipe(autoprefixer({browsers: ['last 5 versions']}))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('prod/css'));
 });
 
 
 /**
- * Minify our JS.
+ * Minify JS.
  */
 gulp.task('prod-scripts', function() {
     return gulp.src('js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('prod/js'));
+    .pipe(uglify())
+    .pipe(gulp.dest('prod/js'));
 });
 
 
@@ -182,7 +162,7 @@ gulp.task('prod', function() {
     runSequence('prod-clean', 
                 'prod-move-files', 
                 'prod-move-dir', 
-                'prod-autoprefix', 
+                'prod-styles', 
                 'prod-scripts'
             );
 })
