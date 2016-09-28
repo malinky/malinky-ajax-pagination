639833fe48b70e31ac1b7453e31a5395d170fdce
gordielachance <Gordie@mbp-de-gordie.home>
2016-09-29 01:48:53 +0200

    update uninstall routine

--- uninstall.php
+++ uninstall.php
@@ -3,15 +3,6 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
- 
-function malinky_ajax_pagination_settings_count_settings()
-{
-    for ( $x = 1; ; $x++ ) {
-        if ( ! get_option( '_malinky_ajax_pagination_settings_' . $x ) ) return --$x;
-    }
-}
 
-$total_settings = malinky_ajax_pagination_settings_count_settings();
-for ( $x = 1; $x <= $total_settings; $x++ ) {
-    delete_option( '_malinky_ajax_pagination_settings_' . $x );
-}
+delete_option( '_malinky_ajax_pagination_settings' );
+delete_option('_malinky_ajax_pagination_db' );
