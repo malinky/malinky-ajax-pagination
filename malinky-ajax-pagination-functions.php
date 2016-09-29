41d7b140f688aea2a52e054cd57d6f0a7870836a
gordielachance <Gordie@mbp-de-gordie.home>
2016-09-29 05:24:15 +0200

    new function malinky_get_array_value($keys = null, $array)
    allow to ‘browse’ an array - eg.
    
    $path = array(‘parent’,’child’);
    $option = malinky_get_array_value($path, $array_input);

--- malinky-ajax-pagination-functions.php
+++ malinky-ajax-pagination-functions.php
@@ -1,5 +1,30 @@
 <?php
 
+if ( ! function_exists( 'malinky_get_array_value' ) ) {
+    /**
+     * Get a value in a multidimensional array
+     * http://stackoverflow.com/questions/1677099/how-to-use-a-string-as-an-array-index-path-to-retrieve-a-value
+     * @param type $keys
+     * @param type $array
+     * @return type
+     */
+    function malinky_get_array_value($keys = null, $array){
+        if (!$keys) return $array;
+
+        $keys = (array)$keys;
+        $first_key = $keys[0];
+        if(count($keys) > 1) {
+            if ( isset($array[$keys[0]]) ){
+                return bbppu_get_array_value(array_slice($keys, 1), $array[$keys[0]]);
+            }
+        }elseif (isset($array[$first_key])){
+            return $array[$first_key];
+        }
+
+        return false;
+    }
+}
+
 if ( ! function_exists( 'malinky_load_css' ) ) {
 
     /**
